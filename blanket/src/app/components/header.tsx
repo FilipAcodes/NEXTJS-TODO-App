47d6aea32cf3bc0a1c02ed9126a3c43a0b2e7a17
Filip Altankov <filipaltankov@hotmail.com>
2023-05-09 22:33:06 -0400

    No functionality yet just aesthetics

---
+++ blanket/src/app/components/header.tsx
@@ -0,0 +1,5 @@
+const Header = ({ title }: { title: string }) => {
+  return <h1 style={{ fontSize: "35px" }}>{title}</h1>;
+};
+
+export default Header;
--- blanket/src/app/page.tsx
+++ blanket/src/app/page.tsx
@@ -1,12 +1,17 @@
 import Container from "./components/container";
 import BackgroundImage from "./components/backgroundImage";
+import Header from "./components/header";
 
 export default function Home() {
   return (
     <main>
       <BackgroundImage image="https://images.pexels.com/photos/2736499/pexels-photo-2736499.jpeg?cs=srgb&dl=pexels-content-pixie-2736499.jpg&fm=jpg" />
       <Container>
-        <p>This is inside?</p>
+        <Header title="Add your to-do!" />
+        <form>
+          <input type="text" />
+          <button type="submit">Add!</button>
+        </form>
       </Container>
     </main>
   );
