16799a0bd1517b616f2a48d7092c5a9b850a7bfa
Filip Altankov <filipaltankov@hotmail.com>
2023-05-11 01:01:51 -0400

    usestate implementation

--- blanket/src/app/components/rightcontainer.tsx
+++ blanket/src/app/components/rightcontainer.tsx
@@ -1,11 +1,19 @@
+"use client";
 import { useState } from "react";
 
 const Rightcontainer = () => {
+  const [text, setText] = useState("");
+
   return (
     <div>
       <h1>Enter your todos!</h1>
       <form>
-        <input type="text" placeholder="Enter to-dos!" />
+        <input
+          onChange={(e) => setText(e.target.value)}
+          type="text"
+          placeholder="Enter to-dos!"
+          value={text}
+        />
         <button type="submit">Add!</button>
       </form>
     </div>
