5a94f1d878ea5f9d11a66e8c3ebcc91db67154ac
Filip Altankov <filipaltankov@hotmail.com>
2023-05-14 22:02:33 -0400

    Delete function added

--- blanket/app/leftcontainer/leftcontainer.tsx
+++ blanket/app/leftcontainer/leftcontainer.tsx
@@ -4,19 +4,33 @@ import axios from "axios";
 import { useEffect } from "react";
 const Leftcontainer = () => {
   const { tasks, setTasks } = useGlobalContext();
+
   useEffect(() => {
     axios.get("/api/GetTask").then((res) => {
       setTasks(res.data.data);
     });
   }, []);
-  if (!tasks) return <h1>Loading...</h1>;
 
+  const handleDeleteTask = (taskId: string) => {
+    axios.delete(`/api/DeleteTask/${taskId}`).then((res) => {
+      axios.get("/api/GetTask").then((res) => {
+        setTasks(res.data.data);
+      });
+    });
+  };
+
+  if (!tasks) return <h1>Loading...</h1>;
   return (
     <div>
       <h1>Your to-dos will appear here!</h1>
       <ul>
-        {tasks.map((e, index) => {
-          return <li key={index}>{e.task}</li>;
+        {tasks.map((e) => {
+          return (
+            <li key={e._id}>
+              {e.task}
+              <button onClick={() => handleDeleteTask(e._id)}>X</button>
+            </li>
+          );
         })}
       </ul>
     </div>
