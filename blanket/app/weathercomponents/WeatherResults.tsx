dd9eb133d10a3a261ed9da417c60879d24783b06
Filip Altankov <filipaltankov@hotmail.com>
2023-06-04 18:04:59 -0400

    added images

--- blanket/app/weathercomponents/WeatherResults.tsx
+++ blanket/app/weathercomponents/WeatherResults.tsx
@@ -18,11 +18,15 @@ const WeatherResults: React.FC<WeatherData> = ({ weather }) => {
   let imageSrc = null;
 
   if (weather.weather[0].description.includes("cloud")) {
-    imageSrc = `https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png`;
+    imageSrc = `https://cdn-icons-png.flaticon.com/512/252/252035.png`;
   } else if (weather.weather[0].description.includes("snow")) {
     imageSrc = `https://cdn.icon-icons.com/icons2/2050/PNG/512/snow_icon_124403.png`;
   } else if (weather.weather[0].description.includes("sunny")) {
     imageSrc = `https://cdn.icon-icons.com/icons2/1370/PNG/512/if-weather-3-2682848_90785.png`;
+  } else if (weather.weather[0].description.includes("thunder")) {
+    imageSrc = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2Oq1tfXv6vSEOPmCi5Et1R8BABDbOsu_0SPxUdWg&s`;
+  } else if (weather.weather[0].description.includes("rain")) {
+    imageSrc = `https://t4.ftcdn.net/jpg/03/38/74/43/360_F_338744374_c8v4RyKnToRWqPA4SalFf8ktaMQA48QW.jpg`;
   }
   return (
     <div>
